a9ceedd008e0897584de8d413bff16e32d5300d4
Tuntufye4 <tuntumwanyongo@gmail.com>
2025-12-02 15:42:02 +0200

    updated allowed host

--- poultryapi/settings.py
+++ poultryapi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hu6qqte)68eu&ng)fd##z%k^w_xb7mq+px=%c3cs+u0jf(51e5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True                 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['poultry-management-api.onrender.com','127.0.0.1', 'localhost']
 
 
 # Application definition
